ec283b06d62cea4bd8d8676b17c22833dc02266c
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-06-26 14:12:07 +0300

    Basic signalr implementation

---
+++ SignalRLearning.Api/Hubs/NotificationHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRLearning.Api.Hubs
+{
+    public class NotificationHub:Hub
+    {
+        public static List<string> Names { get; set; }
+        public async Task SendMessage(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveMessage", name);
+        }
+        public async Task GetMessages()
+        {
+            await Clients.All.SendAsync("ReceiveMessages", Names);
+        }
+    }
+}
